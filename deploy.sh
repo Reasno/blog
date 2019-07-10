215f875ca8bc526bfd1334c0b1ad9da2fbd23faa
reasno <guxi99@gmail.com>
2019-07-11 07:58:15 +0800

    rebuilding site Thu Jul 11 07:56:59 CST 2019

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,13 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-# Commit changes.
+# Default message
 msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
 
+# Commit to main repository
 git add .
 
 git pull -X theirs
@@ -21,11 +25,6 @@ cd public
 # Add changes to git.
 git add .
 
-# Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
 git commit -m "$msg"
 
 # Push source and build repos.
